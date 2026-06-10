bdc4d0c65bfad60e28ab33ec87d7b09b44522a98
Rohit <rohitsingh28205@gmail.com>
2026-06-10 14:02:31 +0530

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import SessionWrapper from "@/components/SessionWrapper";
+import Cartbtn from "@/components/Cartbtn";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +31,14 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex  bg-black/85 w-screen flex-col">
+      <SessionWrapper>
+      <Navbar />
+      <Cartbtn />
+      {children}
+      <Footer />
+      </SessionWrapper>
+      </body>
     </html>
   );
 }
